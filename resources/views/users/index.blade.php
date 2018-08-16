0fd5326b2026a8b930c1fc59af89e984f5c23c79
Chihiro Suzuki <chiicheer.net@gmail.com>
2018-08-16 22:38:12 +0900

    haha

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,23 @@
+<div class="container">
+    <h1>はっはー</h1>
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>{{ __('ID') }}</th>
+                    <th>{{ __('Name') }}</th>
+                    <th>{{ __('Company') }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr>
+                        <td>{{ $user->id }}</td>
+                        <td><a href="{{ url('users/'.$user->id) }}">{{ $user->name }}</a></td>
+                        <td><a href="{{ url('users/'.$user->id) }}">{{ $user->company }}</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
